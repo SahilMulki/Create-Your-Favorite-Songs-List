b63b68eb2d1ed2b578be379115fece10bb972886
Sahil <sahilmulki@sahils-mbp-4.lan>
2025-08-02 21:45:36 -0400

    first commit

---
+++ music-list/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <div className="text-red-500 font-mono text-[300px]">
+      Hello world
+      </div>
+    </>
+  )
+}
